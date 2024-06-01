9cead0e7303894c061ae67081f9812fbe24153e1
Andrzej Melcer <an.melcer@gmail.com>
2024-06-01 10:33:01 +0200

    Add summary

--- src/components/stepper/summary.tsx
+++ src/components/stepper/summary.tsx
@@ -1,9 +1,18 @@
-import { useStepperContext } from "./context"
+import { Switch, Typography } from 'antd';
+import { useStepperContext } from './context';
 
 export default function Summary() {
-    const { canGoToShop, cuisine, ingredients } = useStepperContext()
+  const { canGoToShop, cuisine, ingredients, onCanGoToShopChange } = useStepperContext();
 
-
-
-    return <>{JSON.stringify({ canGoToShop, cuisine, ingredients })}</>
+  return (
+    <>
+      <Typography.Title level={3}>Podsumowując twoje danie 🌮🍕🥪</Typography.Title>
+      <Typography.Text>Masz ochotę na kuchnię z kategorii: {cuisine.join(', ')}</Typography.Text> <br />
+      <Typography.Text>Posiadasz te składniki: {ingredients.join(', ')}</Typography.Text> <br />
+      <Typography.Text>
+        <Switch checked={canGoToShop} onChange={onCanGoToShopChange} size="small" /> i {!canGoToShop && 'nie'} chcesz
+        pójść do sklepu po dodatkowe składniki
+      </Typography.Text>
+    </>
+  );
 }
